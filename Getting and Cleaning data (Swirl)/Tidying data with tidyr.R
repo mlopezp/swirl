3c7abc5612bfbb3f753574d11cc10a0584b9daab
Mau <mauilopezp@gmail.com>
2016-05-26 00:20:18 -0400

    Finished lesson

--- Getting and Cleaning data (Swirl)/Tidying data with tidyr.R
+++ Getting and Cleaning data (Swirl)/Tidying data with tidyr.R
@@ -8,4 +8,154 @@ library(tidyr)
 
 gather(students, sex, count, -grade)
 
-res <- gather(students2, sex_class, count)
+#Let's start by using gather() to stack the columns of students2, like we just did with students. This time, name the 'key' column sex_class and the 'value' column count. Save the result to a new variable called res. Consult ?gather again if you need help.
+
+res <- gather(students2, sex_class, count, -grade)
+
+#Print res
+res
+
+#Call separate() on res to split the sex_class column into sex and class. You only need to specify the first three arguments: data= res, col = sex_class, into = c("sex", "class"). You don't have to provide the argument names as long as they are in the correct order.
+
+separate(data = res, col = sex_class, into = c("sex", "class"))
+
+# Repeat your calls to gather() and separate(), but this time
+# use the %>% operator to chain the commands together without
+# storing an intermediate result.
+#
+# If this is your first time seeing the %>% operator, check
+# out ?chain, which will bring up the relevant documentation.
+# You can also look at the Examples section at the bottom
+# of ?gather and ?separate.
+#
+# The main idea is that the result to the left of %>%
+# takes the place of the first argument of the function to
+# the right. Therefore, you OMIT THE FIRST ARGUMENT to each
+# function.
+#
+students2 %>%
+  gather(sex_class, count, -grade ) %>%
+  separate(col = sex_class , into =  c("sex", "class")) %>%
+  print
+
+#A third symptom of messy data is when variables are stored in both rows and columns. students3 provides an example of this. Print students3 to the console.
+students3
+
+students3 %>%
+  gather(class, grade, class1:class5, na.rm = TRUE) %>%
+  print
+
+# This script builds on the previous one by appending
+# a call to spread(), which will allow us to turn the
+# values of the test column, midterm and final, into
+# column headers (i.e. variables).
+#
+# You only need to specify two arguments to spread().
+# Can you figure out what they are? (Hint: You don't
+# have to specify the data argument since we're using
+# the %>% operator.
+#
+students3 %>%
+  gather(class, grade, class1:class5, na.rm = TRUE) %>%
+  spread( test, grade) %>%
+  print
+
+# We want the values in the class columns to be
+# 1, 2, ..., 5 and not class1, class2, ..., class5.
+#
+# Use the mutate() function from dplyr along with
+# extract_numeric(). Hint: You can "overwrite" a column
+# with mutate() by assigning a new value to the existing
+# column instead of creating a new column.
+#
+# Check out ?mutate and/or ?extract_numeric if you need
+# a refresher.
+#
+students3 %>%
+  gather(class, grade, class1:class5, na.rm = TRUE) %>%
+  spread(test, grade) %>%
+  ### Call to mutate() goes here %>%
+  mutate(class = extract_numeric(class)) %>%
+print
+
+# Complete the chained command below so that we are
+# selecting the id, name, and sex column from students4
+# and storing the result in student_info.
+#
+student_info <- students4 %>%
+  select( id, name, sex ) %>%
+  print
+
+# Add a call to unique() below, which will remove
+# duplicate rows from student_info.
+#
+# Like with the call to the print() function below,
+# you can omit the parentheses after the function name.
+# This is a nice feature of %>% that applies when
+# there are no additional arguments to specify.
+#
+student_info <- students4 %>%
+  select(id, name, sex) %>%
+  unique %>%
+  print
+
+# select() the id, class, midterm, and final columns
+# (in that order) and store the result in gradebook.
+#
+gradebook <- students4 %>%
+  select(id, class, midterm, final) %>%
+print
+
+#Use dplyr's mutate() to add a new column to the passed table. The column should be called status and the value, "passed" (a character string), should be the same for all students. 'Overwrite' the current version of passed with the new one.
+
+passed <- mutate(passed, status = "passed")
+
+#Now, do the same for the failed table, except the status column should have the value "failed" for all students.
+
+failed <- mutate(failed, status = "failed")
+
+#Now, pass as arguments the passed and failed tables (in order) to the dplyr function bind_rows(), which will join them together into a single unit. Check ?bind_rows if you need help.
+
+bind_rows(passed, failed)
+
+# Accomplish the following three goals:
+#
+# 1. select() all columns that do NOT contain the word "total",
+# since if we have the male and female data, we can always
+# recreate the total count in a separate column, if we want it.
+# Hint: Use the contains() function, which you'll
+# find detailed in 'Special functions' section of ?select.
+#
+# 2. gather() all columns EXCEPT score_range, using
+# key = part_sex and value = count.
+#
+# 3. separate() part_sex into two separate variables (columns),
+# called "part" and "sex", respectively. You may need to check
+# the 'Examples' section of ?separate to remember how the 'into'
+# argument should be phrased.
+#
+sat %>%
+  select(-contains("total")) %>%
+  gather(part_sex, count, -score_range) %>%
+  separate(part_sex, into = c("part", "sex")) %>%
+  print
+
+# Append two more function calls to accomplish the following:
+#
+# 1. Use group_by() (from dplyr) to group the data by part and
+# sex, in that order.
+#
+# 2. Use mutate to add two new columns, whose values will be
+# automatically computed group-by-group:
+#
+#   * total = sum(count)
+#   * prop = count / total
+#
+sat %>%
+  select(-contains("total")) %>%
+  gather(part_sex, count, -score_range) %>%
+  separate(part_sex, c("part", "sex")) %>%
+  group_by(part, sex) %>%
+  mutate(total = sum(count),
+         prop = count / total) %>%
+  print
